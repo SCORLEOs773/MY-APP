a415acb4d81b97c37158fed96a228b071c223d0c
SUKHAD SHARMA <mykeyman777@gmail.com>
2023-06-17 15:21:37 +0530

    Updates

---
+++ precache-manifest.3892b8032d622a6053f1acc8f1e2db69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f8683d4271740af34d7",
+    "url": "/MY-APP/static/css/main.1a55a348.chunk.css"
+  },
+  {
+    "revision": "1f8683d4271740af34d7",
+    "url": "/MY-APP/static/js/main.1f8683d4.chunk.js"
+  },
+  {
+    "revision": "66d3df16377a19ed444f",
+    "url": "/MY-APP/static/js/1.66d3df16.chunk.js"
+  },
+  {
+    "revision": "1359abe67b62ee977b7b",
+    "url": "/MY-APP/static/js/2.1359abe6.chunk.js"
+  },
+  {
+    "revision": "2224b94ec85b12028619",
+    "url": "/MY-APP/static/js/runtime~main.2224b94e.js"
+  },
+  {
+    "revision": "e15701f37ce6b61f83f760b8acc70248",
+    "url": "/MY-APP/index.html"
+  }
+];
